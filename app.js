5ef3e63167132ada02362e7eeda88d4dc3996e67
Avin Umar <avinumar9@gmail.com>
2025-11-12 23:15:45 +0530

    ejs mate implement

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Product = require("./models/product");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/toFarm";
 
@@ -20,6 +21,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req,res)=>{
     res.send("Hi I am root");
@@ -59,6 +62,8 @@ app.delete("/products/:id", async(req,res) =>{
     res.redirect("/products");
 })
 
+
+// Delete Route
 app.post("/products", async(req,res) =>{
     const newProduct = new Product(req.body.product);
     await newProduct.save();
